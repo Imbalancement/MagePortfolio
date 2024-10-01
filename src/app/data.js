ace96b0cabfb88505fdba99224726bd0b331f479
Imbalancement <138077219+Imbalancement@users.noreply.github.com>
2024-10-01 18:33:16 -0400

    Add files via upload

---
+++ src/app/data.js
@@ -0,0 +1,74 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "EcoTracker",
+    description: "Track your carbon footprint",
+    date: "2024",
+    demoLink: "https://chatapp-8vkh.onrender.com/login",
+  },
+  {
+    id: 2,
+    name: "NovaCart",
+    description: "For all your shopping needs.",
+    date: "2024",
+    demoLink: "https://novacart-44g8.onrender.com/",
+  },
+  {
+    id: 3,
+    name: "ChatSphere",
+    description: "Chat with friends and make a few new ones!",
+    date: "",
+    demoLink: "https://chatapp-8vkh.onrender.com/login",
+  },
+  
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/Imbalancement",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/corey-williams-88987727b/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Discord",
+    link: "https://discordapp.com/users/339196150823256064",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "https://coreywilliamsdev.com/CoreyWilliamsResume2.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
